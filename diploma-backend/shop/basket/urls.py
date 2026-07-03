428536a5baa46b14029229056318d02b9bfa5ba3
kil-2310 <homakove600@gmail.com>
2026-07-03 19:38:23 +0300

    Updated some files

--- diploma-backend/shop/basket/urls.py
+++ diploma-backend/shop/basket/urls.py
@@ -3,5 +3,5 @@ from .views import BasketView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('basket/', csrf_exempt(BasketView.as_view()), name='basket'),
+    path('basket', csrf_exempt(BasketView.as_view()), name='basket'),
 ]
