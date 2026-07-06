edc61acf8764b5ace8437cd807d22ed0f510d9d1
kil-2310 <homakove600@gmail.com>
2026-07-06 15:51:07 +0300

    Updated tags app

--- diploma-backend/shop/tags/models.py
+++ diploma-backend/shop/tags/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class Tag(models.Model):
     """Тэг"""
+
+    class Meta:
+        ordering = ('pk', )
 
     name = models.CharField(max_length=255, unique=True)
